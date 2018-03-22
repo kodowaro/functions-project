f804a3dff0b1454cb73b0f0dca4b167c50960dc8
Alexey Merzlyakov <kodowaro@gmail.com>
2018-03-22 22:01:32 +0300

    qe_zadacha

---
+++ QuadraticEquasion/quadratic_equasion.pas
@@ -0,0 +1,18 @@
+program QuadraticEquasion;
+
+uses crt;
+
+type ans_t = array[1..3] of real;
+
+function qe_solve(a, b, c : integer; var ans : ans_t) : boolean;
+begin
+	qe_solve := false;
+end;
+
+function test() : boolean;
+begin
+	test := false;
+end;
+
+BEGIN
+END.
